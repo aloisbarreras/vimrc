cb1928728b0422f8a0ddff8c37e5329742e804d4
Alois Barreras <alois@astronomer.io>
2016-10-26 05:09:58 -0400

    updated install script and now ignoring undodir

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -2,11 +2,6 @@ cd ~/.vim_runtime
 
 echo 'set runtimepath+=~/.vim_runtime
 source ~/.vim_runtime/vimrcs/basic.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-try
-source ~/.vim_runtime/my_configs.vim
-catch
-echo "Missing Files"
-endtry' > ~/.vimrc
+source ~/.vim_runtime/vimrcs/plugins_config.vim' > ~/.vimrc
 
 echo "Installed vim configuration successfully."
